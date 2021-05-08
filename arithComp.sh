ebc8282cbc4ff0a4e46b7e35544411d6679ed874
HajarePratik <pratikhajare09@gmail.com>
2021-05-08 19:20:05 +0530

    Uc6 Computation Dic Done

--- arithComp.sh
+++ arithComp.sh
@@ -1,5 +1,5 @@
 #! /bin/bash -x
-
+declare -A dict
 echo "Welcome to the Arithmatic Computation and Sorting"
 
 read -p "Enter First Number :" a
@@ -7,12 +7,15 @@ read -p "Enter Second Number :" b
 read -p "Enter Third Number :" c
 
 sum=$(echo $a $b $c | awk '{print $1+$2*$3}')
+dict['sum']=$sum
 
 sum2=$(echo $a $b $c | awk '{print $3+$1/$2}')
+dict['sum2']=$sum2
 
 sum3=$(echo $a $b $c | awk '{print $1*$2+$3}')
+dict['sum3']=$sum3
 
 sum4=$(echo $a $b $c | awk '{print $1%$2+$3}')
+dict['sum4']=$sum4
 
-
-
+echo ${dict[@]}
