6393503282ccffd70565332081ba8a241c83b85d
HajarePratik <pratikhajare09@gmail.com>
2021-05-08 19:29:20 +0530

    Uc6 Computation Dic Done

--- arithComp.sh
+++ arithComp.sh
@@ -18,4 +18,7 @@ dict['sum3']=$sum3
 sum4=$(echo $a $b $c | awk '{print $1%$2+$3}')
 dict['sum4']=$sum4
 
-echo ${dict[@]}
+echo "Dictory value :${dict[@]}"
+
+arr=(${dict[@]})
+echo "Value of Array are : ${arr[@]}"
